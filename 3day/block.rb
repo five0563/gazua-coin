2d77f2ab7be6b64802e01d8373516457acc6f5d1
five0563 <38690304+five0563@users.noreply.github.com>
2018-05-02 22:40:53 +0900

    4일차
    4일차_1

--- 3day/block.rb
+++ 3day/block.rb
@@ -1,7 +1,44 @@
 class Blockchain
      def initialize
       @chain = []
+      @trans = [] 
+      @wallet = {}
      end
+
+     def show_all_wallet
+      @wallet
+     end
+     
+
+     def make_a_new_wallet
+      address = SecureRandom.uuid.gsub("-","")
+      @wallet[address] = 1000
+      @wallet
+     end
+	 
+     def make_a_trans(s,r,a)
+     	if @wallet[s].nil?
+     		"보내는 주소가 잘못되었습니다."
+     	elsif @wallet[r].nil?
+            "받는 주소가 잘못되었습니다." 
+        elsif @wallet[s].to_f < a.to_f
+            "코인 없다."   
+        else
+
+         @wallet[s] = @wallet[s].to_f - a.to_f
+         @wallet[r] = @wallet[r].to_f + a.to_f
+
+     	 trans = {
+     	   "sender" => s,
+     	   "receiver" => r,
+     	   "amount" => a
+     	 }
+         @trans << trans
+         @trans
+        end 
+     end	 
+
+
 	 def mining
 
 	   history = []
@@ -13,24 +50,25 @@ class Blockchain
 	   end while hashed[0..3]!= "0000"
 	   
         block = {
-        	"index" => @chain.size+ 1, 
+        	"index" => @chain.size + 1, 
         	"time" => Time.now, 
         	"nonce" => nonce, 
-        	"previous_address" => Digest::SHA256.hexdigest(last_block.to_s)
+        	"previous_address" => Digest::SHA256.hexdigest(last_block.to_s),
+        	"transactions" => @trans
         }   
        
+        @trans = []
         @chain << block
+        block
+        end
 
-	   history.size
-
-	 end
-     
+	     
      def last_block
        @chain[-1]
      end 
   
-  def all_chains
-  	@chain
-  end
+     def all_chains
+  	   @chain
+     end
 
 end
--- 3day/server.rb
+++ 3day/server.rb
@@ -5,22 +5,38 @@ b = Blockchain.new
 
 get '/' do
 
-  message = ""
+  message = "<center>"
 
   b.all_chains.each do |a|
 
-  message << a['index'].to_s + "<br>"
-
+   message << "번호는:" + a["index"].to_s + "<br>"
+   message << "Nonce는:" + a["nonce"].to_s + "<br>"
+   message << "시간은:" + a["time"].to_s + "<br>"
+   message << "앞주소는:" + a["previous_address"].to_s + "<br>"
+   message << "내주소는:" + Digest::SHA256.hexdigest(a.to_s) + "<br>"
+   message << "transactions:" + a["transactions"].to_s + "<br>"
+   message << "<hr>"
   end
 
 
- message
+ message << "</center>"
  end
+ 
 
 
  get '/mine' do
   b.mining.to_s
+ end
+
+ get '/trans' do
+  b.make_a_trans(params["sender"], params["recv"], params["amount"]).to_s	
+ end
  
-	
-	
-end
+ get '/new_wallet' do
+  b.make_a_new_wallet.to_s
+ end
+
+ get '/all_wallet' do
+  b.show_all_wallet.to_s
+ end
+
